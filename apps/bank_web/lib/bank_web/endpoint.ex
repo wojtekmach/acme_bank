393115ae92476fde0c98698561e9b96437a9a3b7
Wojtek Mach <wojtek@wojtekmach.pl>
2016-05-27 22:44:05 +0200

    bank_web: Remove web/channels for now

--- apps/bank_web/lib/bank_web/endpoint.ex
+++ apps/bank_web/lib/bank_web/endpoint.ex
@@ -1,8 +1,6 @@
 defmodule BankWeb.Endpoint do
   use Phoenix.Endpoint, otp_app: :bank_web
 
-  socket "/socket", BankWeb.UserSocket
-
   # Serve at "/" the static files from "priv/static" directory.
   #
   # You should set gzip to true if you are running phoenix.digest
