b94a44e4ef41ed8b9d646e7dc8d6cf2d3d951663
Wojtek Mach <wojtek@wojtekmach.pl>
2016-06-06 20:11:50 +0200

    bank_web: Run seeds only in dev and don't manually delete records

--- apps/bank_web/priv/repo/seeds.exs
+++ apps/bank_web/priv/repo/seeds.exs
@@ -1,8 +1,8 @@
-BankWeb.Repo.delete_all(BankWeb.Account)
-BankWeb.Repo.delete_all(BankWeb.Customer)
-BankWeb.Repo.delete_all(BankWeb.Transaction)
+if Mix.env == :dev do
 
-alice = BankWeb.Customer.build(%{username: "alice"}) |> BankWeb.Repo.insert!
-{:ok, _} = BankWeb.Deposit.build(alice, 10_00) |> BankWeb.Ledger.write
+  alice = BankWeb.Customer.build(%{username: "alice"}) |> BankWeb.Repo.insert!
+  {:ok, _} = BankWeb.Deposit.build(alice, 10_00) |> BankWeb.Ledger.write
 
-BankWeb.Customer.build(%{username: "bob"}) |> BankWeb.Repo.insert!
+  BankWeb.Customer.build(%{username: "bob"}) |> BankWeb.Repo.insert!
+
+end
