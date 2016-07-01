1aa3f85aa01a305a35cd5cc78c9e4a6ddb6977d8
Wojtek Mach <wojtek@wojtekmach.pl>
2016-07-02 00:42:26 +0200

    umbrella: Cleanup

--- mix.exs
+++ mix.exs
@@ -8,18 +8,6 @@ defmodule BankUmbrella.Mixfile do
      deps: deps()]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options.
-  #
-  # Dependencies listed here are available only for this project
-  # and cannot be accessed from applications inside the apps folder
   defp deps do
     []
   end
