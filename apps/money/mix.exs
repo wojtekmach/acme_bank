b0a2fb44ec925806b791ac2651ba621349239cde
Wojtek Mach <wojtek@wojtekmach.pl>
2016-07-02 00:41:27 +0200

    money: Cleanup

--- apps/money/mix.exs
+++ apps/money/mix.exs
@@ -14,26 +14,10 @@ defmodule Money.Mixfile do
      deps: deps()]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
   def application do
     [applications: [:logger]]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # To depend on another app inside the umbrella:
-  #
-  #   {:myapp, in_umbrella: true}
-  #
-  # Type "mix help deps" for more examples and options
   defp deps do
     []
   end
