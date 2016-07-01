c0ee21ac48de85cebf098bf11a8fb8be505736e0
Wojtek Mach <wojtek@wojtekmach.pl>
2016-07-02 00:25:49 +0200

    bank_web: Add alias for ecto.seed

--- apps/bank_web/mix.exs
+++ apps/bank_web/mix.exs
@@ -53,7 +53,8 @@ defmodule BankWeb.Mixfile do
   #
   # See the documentation for `Mix` for more info on aliases.
   defp aliases do
-    ["ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
+    ["ecto.setup": ["ecto.create", "ecto.migrate", "ecto.seed"],
+     "ecto.seed": ["run priv/repo/seeds.exs"],
      "ecto.reset": ["ecto.drop", "ecto.setup"],
      "test": ["ecto.create --quiet", "ecto.migrate", "test"]]
   end
