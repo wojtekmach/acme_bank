ef45dc1c66fe4c6ac2e215e7589808a52bacda02
Wojtek Mach <wojtek@wojtekmach.pl>
2016-07-02 11:19:38 +0200

    money: Support Ecto.Type only when Ecto is loaded

--- apps/money/lib/money.ex
+++ apps/money/lib/money.ex
@@ -53,22 +53,24 @@ defmodule Money do
     "$#{dollars}.#{cents}"
   end
 
-  @behaviour Ecto.Type
+  if Code.ensure_loaded?(Ecto.Type) do
+    @behaviour Ecto.Type
 
-  def type, do: :moneyz
+    def type, do: :moneyz
 
-  # TODO:
-  def cast(_), do: :error
+    # TODO:
+    def cast(_), do: :error
 
-  def load({cents, currency}) when is_integer(cents) and is_binary(currency) do
-    {:ok, %Money{cents: cents, currency: currency}}
+    def load({cents, currency}) when is_integer(cents) and is_binary(currency) do
+      {:ok, %Money{cents: cents, currency: currency}}
+    end
+    def load(_), do: :error
+
+    def dump(%Money{cents: cents, currency: currency}) do
+      {:ok, {cents, currency}}
+    end
+    def dump(_), do: :error
   end
-  def load(_), do: :error
-
-  def dump(%Money{cents: cents, currency: currency}) do
-    {:ok, {cents, currency}}
-  end
-  def dump(_), do: :error
 end
 
 defimpl Inspect, for: Money do
