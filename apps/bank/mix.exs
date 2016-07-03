0938990711acec3ff86307b14114aa5832cccc2a
Wojtek Mach <wojtek@wojtekmach.pl>
2016-07-03 02:58:13 +0200

    bank: Initial commit

---
+++ apps/bank/mix.exs
@@ -0,0 +1,28 @@
+defmodule Bank.Mixfile do
+  use Mix.Project
+
+  def project do
+    [app: :bank,
+     version: "0.1.0",
+     build_path: "../../_build",
+     config_path: "../../config/config.exs",
+     deps_path: "../../deps",
+     lockfile: "../../mix.lock",
+     elixir: "~> 1.4-dev",
+     build_embedded: Mix.env == :prod,
+     start_permanent: Mix.env == :prod,
+     deps: deps()]
+  end
+
+  # Configuration for the OTP application
+  #
+  # Type "mix help compile.app" for more information
+  def application do
+    [applications: [:logger],
+     mod: {Bank.Application, []}]
+  end
+
+  defp deps do
+    []
+  end
+end
---
+++ apps/bank/test/bank_test.exs
@@ -0,0 +1,7 @@
+defmodule BankTest do
+  use ExUnit.Case
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
