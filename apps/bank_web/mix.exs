03bc0f6e9a8b5eea1b5303795f13fc7d6b3e7231
Wojtek Mach <wojtek@wojtekmach.pl>
2016-07-03 02:51:18 +0200

    bank_web: Update deps

--- apps/bank_web/mix.exs
+++ apps/bank_web/mix.exs
@@ -34,8 +34,8 @@ defmodule BankWeb.Mixfile do
   #
   # Type `mix help deps` for examples and options.
   defp deps do
-    [{:phoenix, "~> 1.2.0-rc"},
-     {:phoenix_pubsub, "~> 1.0.0-rc"},
+    [{:phoenix, "~> 1.2"},
+     {:phoenix_pubsub, "~> 1.0.0"},
      {:phoenix_ecto, "~> 3.0"},
      {:postgrex, ">= 0.0.0"},
      {:phoenix_html, "~> 2.5"},
