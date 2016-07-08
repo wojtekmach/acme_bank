e4824cd60e584f013298c3e6308d590024fc7114
Wojtek Mach <wojtek@wojtekmach.pl>
2016-07-08 21:23:02 +0200

    umbrella: Add mix aliases

--- mix.exs
+++ mix.exs
@@ -5,10 +5,18 @@ defmodule BankUmbrella.Mixfile do
     [apps_path: "apps",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
-     deps: deps()]
+     deps: deps(),
+     aliases: aliases()]
   end
 
   defp deps do
     []
+  end
+
+  defp aliases do
+    ["ecto.setup": ["ecto.create", "ecto.migrate", "ecto.seed"],
+     "ecto.seed": ["run apps/bank/priv/repo/seeds.exs"],
+     "ecto.reset": ["ecto.drop", "ecto.setup"],
+     "test": ["ecto.create --quiet", "ecto.migrate", "test"]]
   end
 end
