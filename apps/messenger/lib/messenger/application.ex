530cfb0563768716fd87f4b3d020a3cfe3a59867
Wojtek Mach <wojtek@wojtekmach.pl>
2016-07-08 21:35:13 +0200

    messenger: Add Messenger.Application

---
+++ apps/messenger/lib/messenger/application.ex
@@ -0,0 +1,13 @@
+defmodule Messenger.Application do
+  use Application
+
+  def start(_type, _args) do
+    import Supervisor.Spec, warn: false
+
+    children = [
+    ]
+
+    opts = [strategy: :one_for_one, name: Messenger.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+end
--- apps/messenger/mix.exs
+++ apps/messenger/mix.exs
@@ -14,27 +14,11 @@ defmodule Messenger.Mixfile do
      deps: deps()]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
   def application do
     [applications: [:logger],
-     mod: {Messenger, []}]
+     mod: {Messenger.Application, []}]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # To depend on another app inside the umbrella:
-  #
-  #   {:myapp, in_umbrella: true}
-  #
-  # Type "mix help deps" for more examples and options
   defp deps do
     []
   end
