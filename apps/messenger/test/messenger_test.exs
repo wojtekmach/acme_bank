b601728a860b8a14d4d3617c49ab9e026f1e1a40
Wojtek Mach <wojtek@wojtekmach.pl>
2016-07-08 21:45:41 +0200

    messenger: Add test

--- apps/messenger/test/messenger_test.exs
+++ apps/messenger/test/messenger_test.exs
@@ -2,7 +2,25 @@ defmodule MessengerTest do
   use ExUnit.Case
   doctest Messenger
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  setup do
+    :ok = Messenger.Test.setup()
+    :ok
+  end
+
+  test "messenger" do
+    :ok = Messenger.send("alice", "subject 1", "body 1")
+    :ok = Messenger.send("alice", "subject 2", "body 2")
+    :ok = Messenger.send("bob", "subject 3", "body 3")
+    
+    assert Messenger.Test.subjects_for("alice") == ["subject 2", "subject 1"]
+    assert Messenger.Test.messages_for("alice") == [
+      {"subject 2", "body 2"},
+      {"subject 1", "body 1"},
+    ]
+
+    assert Messenger.Test.subjects_for("bob") == ["subject 3"]
+    assert Messenger.Test.messages_for("bob") == [
+      {"subject 3", "body 3"},
+    ]
   end
 end
