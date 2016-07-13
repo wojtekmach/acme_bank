7201601fa158aa8f377d30ff108cd5d20238686d
Wojtek Mach <wojtek@wojtekmach.pl>
2016-07-13 09:17:35 +0200

    bank_web: Use plug master to get the new errors page

--- apps/bank_web/mix.exs
+++ apps/bank_web/mix.exs
@@ -42,6 +42,7 @@ defmodule BankWeb.Mixfile do
      {:phoenix_live_reload, "~> 1.0", only: :dev},
      {:gettext, "~> 0.11"},
      {:cowboy, "~> 1.0"},
+     {:plug, github: "elixir-lang/plug", override: true},
 
      {:bank, in_umbrella: true},
      {:messenger, in_umbrella: true}]
