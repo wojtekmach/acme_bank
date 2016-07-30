c52e60cf559927394c6fb84330bca98d59eed70b
Wojtek Mach <wojtek@wojtekmach.pl>
2016-07-30 09:42:47 +0200

    all: Update to Elixir v1.3.2

--- apps/money/mix.exs
+++ apps/money/mix.exs
@@ -8,7 +8,7 @@ defmodule Money.Mixfile do
      config_path: "../../config/config.exs",
      deps_path: "../../deps",
      lockfile: "../../mix.lock",
-     elixir: "~> 1.4-dev",
+     elixir: ">= 1.3.2",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps(),
