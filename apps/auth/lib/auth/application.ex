aeea0c1e8bfa63cdecf0c948f5c9afc48ffee526
Wojtek Mach <wojtek@wojtekmach.pl>
2016-07-31 23:07:24 +0200

    auth: Add Auth.Application

---
+++ apps/auth/lib/auth/application.ex
@@ -0,0 +1,12 @@
+defmodule Auth.Application do
+  use Application
+
+  def start(_type, _args) do
+    import Supervisor.Spec, warn: false
+
+    children = []
+
+    opts = [strategy: :one_for_one, name: Auth.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+end
--- apps/auth/mix.exs
+++ apps/auth/mix.exs
@@ -19,22 +19,9 @@ defmodule Auth.Mixfile do
   # Type "mix help compile.app" for more information
   def application do
     [applications: [:logger],
-     mod: {Auth, []}]
+     mod: {Auth.Application, []}]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # To depend on another app inside the umbrella:
-  #
-  #   {:myapp, in_umbrella: true}
-  #
-  # Type "mix help deps" for more examples and options
   defp deps do
     []
   end
