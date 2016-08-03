740c31808983329ec4286bda70aa4980f618eb3e
Wojtek Mach <wojtek@wojtekmach.pl>
2016-08-03 23:53:21 +0200

    auth: Prepare for deployment

---
+++ apps/auth/config/prod.exs
@@ -0,0 +1,9 @@
+use Mix.Config
+
+config :logger, level: :info
+
+config :auth, Auth.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  url: System.get_env("DATABASE_URL"),
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
+  ssl: true
