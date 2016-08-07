6dcf31b45184d33f8deb0f78c03847e596b30460
Wojtek Mach <wojtek@wojtekmach.pl>
2016-08-07 20:34:53 +0200

    master_proxy: Fix build

--- apps/master_proxy/lib/master_proxy.ex
+++ apps/master_proxy/lib/master_proxy.ex
@@ -4,7 +4,7 @@ defmodule MasterProxy do
   def start(_type, _args) do
     import Supervisor.Spec, warn: false
 
-    port = System.get_env("PORT") |> String.to_integer
+    port = (System.get_env("PORT") || "3333") |> String.to_integer
     cowboy = Plug.Adapters.Cowboy.child_spec(:http, MasterProxy.Plug, [], [port: port])
 
     children = [
