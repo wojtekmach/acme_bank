dcb28e239adc9d57d7f519b6e49b9d05788d9972
Wojtek Mach <wojtek@wojtekmach.pl>
2016-12-27 18:01:12 +0100

    money: Fix negative amounts handling

--- apps/money/lib/money.ex
+++ apps/money/lib/money.ex
@@ -21,8 +21,8 @@ defmodule Money do
       iex> Money.add(~M"10 USD", ~M"20 USD")
       ~M"30.00 USD"
 
-      iex> Kernel.to_string(~M"10 USD")
-      "10.00 USD"
+      iex> Kernel.to_string(~M"-10.50 USD")
+      "-10.50 USD"
 
       iex> inspect(~M"10 USD")
       "~M\"10.00 USD\""
@@ -30,10 +30,10 @@ defmodule Money do
       iex> Money.parse("10 USD")
       {:ok, ~M"10 USD"}
       iex> Money.parse("10.1 USD")
-      {:error, :invalid_cents}
+      :error
 
       iex> ~M"10.001 USD"
-      ** (ArgumentError) invalid_cents
+      ** (ArgumentError) invalid string: "10.001 USD"
 
   """
 
@@ -42,52 +42,47 @@ defmodule Money do
   def new(str) when is_binary(str) do
     case parse(str) do
       {:ok, money} -> money
-      {:error, reason} -> raise ArgumentError, "#{reason}"
+      :error -> raise ArgumentError, "invalid string: #{inspect str}"
     end
   end
 
   def parse(str) when is_binary(str) do
-    [value, currency] = String.split(str, " ")
-
-    {dollars, cents} =
-      case String.split(value, ".") do
-        [dollars, cents] -> {dollars, cents}
-        [dollars] -> {dollars, "00"}
-      end
-
-    cond do
-      String.length(cents) != 2 ->
-        {:error, :invalid_cents}
-
-      String.length(currency) != 3 ->
-        {:error, :invalid_currency}
-
-      true ->
-        cents = String.to_integer(dollars) * 100 + String.to_integer(cents)
-        {:ok, %Money{cents: cents, currency: currency}}
+    case Regex.run(~r/\A(-?)(\d+)(\.(\d{2}))?\ ([A-Z]{3})\z/, str) do
+      [_, sign, dollars, _, cents, currency] ->
+        do_parse(sign, dollars, cents, currency)
+      _ -> :error
     end
   end
 
-  def sigil_M(str, _opts) do
-    new(str)
+  defp do_parse(sign, dollars, "", currency),
+    do: do_parse(sign, dollars, "00", currency)
+  defp do_parse(sign, dollars, cents, currency) do
+    sign = if sign == "-", do: -1, else: 1
+    cents = sign * (String.to_integer(dollars) * 100 + String.to_integer(cents))
+    {:ok, %Money{cents: cents, currency: currency}}
   end
+
+  def sigil_M(str, _opts),
+    do: new(str)
 
   def add(%Money{cents: left_cents, currency: currency},
           %Money{cents: right_cents, currency: currency}) do
     %Money{cents: left_cents + right_cents, currency: currency}
   end
 
-  def to_string(%Money{cents: cents, currency: currency}) do
+  def to_string(%Money{cents: cents, currency: currency}) when cents >= 0 do
     {dollars, cents} = {div(cents, 100), rem(cents, 100)}
     cents = :io_lib.format("~2..0B", [cents]) |> IO.iodata_to_binary
     "#{dollars}.#{cents} #{currency}"
   end
+  def to_string(%Money{cents: cents, currency: currency}) do
+    "-" <> Money.to_string(%Money{cents: -cents, currency: currency})
+  end
 end
 
 defimpl Inspect, for: Money do
-  def inspect(money, _opts) do
-    "~M\"#{money}\""
-  end
+  def inspect(money, _opts),
+    do: "~M\"#{money}\""
 end
 
 defimpl String.Chars, for: Money do
--- apps/money/test/money_test.exs
+++ apps/money/test/money_test.exs
@@ -1,4 +1,15 @@
 defmodule MoneyTest do
   use ExUnit.Case
   doctest Money, import: true
+  import Money, only: [sigil_M: 2]
+
+  test "works" do
+    assert ~M"10 USD"    == %Money{cents: 10_00, currency: "USD"}
+    assert ~M"10.00 USD" == %Money{cents: 10_00, currency: "USD"}
+    assert ~M"-5.10 USD" == %Money{cents: -5_10, currency: "USD"}
+
+    assert to_string(~M"10 USD")    == "10.00 USD"
+    assert to_string(~M"10.00 USD") == "10.00 USD"
+    assert to_string(~M"-5.10 USD") == "-5.10 USD"
+  end
 end
